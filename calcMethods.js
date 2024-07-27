ea2bfadd9dcfa8efa903d0702d7a3903bc59267b
shangwang <wangshangming10@gmail.com>
2024-07-27 09:41:02 -0400

    create variables for first number, operator and second number

--- calcMethods.js
+++ calcMethods.js
@@ -1,15 +1,20 @@
 function add(a,b){
-
+ return a + b
 }
 
 function subtract(a,b){
-  
+  return a - b
 }
 
 function multiply(a,b){
-  
+  return a * b
 }
 
 function divide(a,b){
-  
+  return a / b
 }
+
+
+let firstNumber //= document.querySelector()
+let calcOp 
+let secondNumber
