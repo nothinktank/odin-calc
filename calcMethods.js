95c39405778fa522c50d2993175bb935d8a385ec
shangwang <wangshangming10@gmail.com>
2024-07-29 20:10:04 -0400

    able to add number 7 consecutively

--- calcMethods.js
+++ calcMethods.js
@@ -17,8 +17,11 @@ function divide(a,b){
 let display = document.querySelector(".display");
 
 let firstAppendage = "";
-let calcOp 
+let operationType = "";
 let secondAppendage = "";
+let numForOperation = "";
+let result = 0;
+let finalFirstNum;
 
 // firstNumber.addEventListener("click", () => {
 //   console.log('a button is clicked')
@@ -26,12 +29,53 @@ let secondAppendage = "";
 // })
 
 function operand(num){
+  if (operationType === ""){
   firstAppendage += num;
   display.textContent = `${firstAppendage}`;
-  
+  console.log("executed")
+  }else{
+    secondAppendage += num
+    display.textContent =  `${secondAppendage}`;
+    //numForOperation = firstAppendage;
+    firstAppendage = "";
+    console.log(`first appended number is ${firstAppendage}`)
+  }
 }
 
 function operator(input){
-  
 
+  if (operationType === ""){
+    operationType = input;
+    finalFirstNum = firstAppendage;
+    console.log(`first operator ${operationType} pressed `);
+  }else {
+    switch (operationType){
+      case "plus": 
+        result = Number(finalFirstNum) + Number(secondAppendage);
+        display.textContent = `${result}`;
+        console.log("adding executed");
+        //assign secondAppendage to finalFirstNum and clear secondAppendage
+        finalFirstNum = result;
+        secondAppendage = "";
+        break;
+      case "minus": 
+        result = Number(finalFirstNum) - Number(secondAppendage);
+        display.textContent = `${result}`;
+        break;
+      case "times": 
+        result = Number(finalFirstNum) * Number(secondAppendage);
+        display.textContent = `${result}`;
+        break;
+      case "divide": 
+        result = Number(finalFirstNum) / Number(secondAppendage);
+        display.textContent = `${result}`;
+        break;
+      case "flipSign": 
+        result = -(Number(secondAppendage));
+        display.textContent = `${result}`;
+        break;
+    }
+    
+  }
+  
 }
